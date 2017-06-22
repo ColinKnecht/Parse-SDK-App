e211d1136d9053cd90a167444ec6efb037b0a6cc
ColinKnecht <cknecht87@gmail.com>
2017-06-22 15:41:58 -0400

    Before Video 12
    Hooking up EditText and Buttons to Java Activities

--- ContactsUdemyCourse/app/src/main/java/com/colinknecht/contactsudemycourse/CreateAccount.java
+++ ContactsUdemyCourse/app/src/main/java/com/colinknecht/contactsudemycourse/CreateAccount.java
@@ -2,12 +2,30 @@ package com.colinknecht.contactsudemycourse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateAccount extends AppCompatActivity {
+    EditText etUsername, etPassword, etRetypePassword, etEmail;
+    Button btnCreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        etUsername = (EditText) findViewById(R.id.et_Username_ca);
+        etPassword = (EditText) findViewById(R.id.et_Password_ca);
+        etRetypePassword = (EditText) findViewById(R.id.et_RetypePassword_ca);
+        etEmail = (EditText) findViewById(R.id.et_Email_ca);
+        btnCreateAccount = (Button) findViewById(R.id.btn_createAccount_ca);
+
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });/////////createAccount onClick
     }
 }
--- ContactsUdemyCourse/app/src/main/java/com/colinknecht/contactsudemycourse/Login.java
+++ ContactsUdemyCourse/app/src/main/java/com/colinknecht/contactsudemycourse/Login.java
@@ -2,12 +2,45 @@ package com.colinknecht.contactsudemycourse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
-
+    EditText etUsername;
+    EditText etPassword;
+    Button btnLogin, btnCreateAccount, btnResetPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etUsername = (EditText) findViewById(R.id.et_Username_login);
+        etPassword = (EditText) findViewById(R.id.et_Password_login);
+        btnLogin = (Button) findViewById(R.id.btn_login_login);
+        btnCreateAccount = (Button) findViewById(R.id.btn_createAccount_Login);
+        btnResetPassword = (Button) findViewById(R.id.btn_resetPassword_login);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });////login onClick
+
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });//createAccount onClick
+
+        btnResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });//resetPassword onClick
+
     }
 }
