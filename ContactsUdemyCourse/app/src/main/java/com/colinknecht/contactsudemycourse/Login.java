438884828b60fd8d469b1b0e83bfae1ad6f3a58f
ColinKnecht <cknecht87@gmail.com>
2017-06-23 14:33:01 -0400

    Before Video 13

--- ContactsUdemyCourse/app/src/main/java/com/colinknecht/contactsudemycourse/Login.java
+++ ContactsUdemyCourse/app/src/main/java/com/colinknecht/contactsudemycourse/Login.java
@@ -1,5 +1,6 @@
 package com.colinknecht.contactsudemycourse;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -31,7 +32,8 @@ public class Login extends AppCompatActivity {
         btnCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(v.getContext(), CreateAccount.class);
+                startActivity(intent);
             }
         });//createAccount onClick
 
