b7e9d2ed7e84392835b5916788a5b95e7b82b0f7
ColinKnecht <cknecht87@gmail.com>
2017-06-29 15:48:44 -0400

    Before Video 23
    Setting up GUI for Add New Contact

--- ContactsUdemyCourse/app/src/main/java/com/colinknecht/contactsudemycourse/NewContact.java
+++ ContactsUdemyCourse/app/src/main/java/com/colinknecht/contactsudemycourse/NewContact.java
@@ -3,9 +3,13 @@ package com.colinknecht.contactsudemycourse;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewContact extends AppCompatActivity {
-
+    EditText etFirstName, etLastName, etPhoneNumber;
+    Button btnAddNewContact;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,5 +17,18 @@ public class NewContact extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Add New Contact");
+
+        etFirstName = (EditText) findViewById(R.id.et_first_name);
+        etLastName = (EditText) findViewById(R.id.et_last_name);
+        etPhoneNumber = (EditText) findViewById(R.id.et_phone_number);
+
+        btnAddNewContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+
 }
