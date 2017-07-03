07905f6d2bbf3a9eb2a881bde28de9e5fd1939f4
ColinKnecht <cknecht87@gmail.com>
2017-07-03 11:48:24 -0400

    Before Video 32

--- ContactsUdemyCourse/app/src/main/java/com/colinknecht/contactsudemycourse/EditContacts.java
+++ ContactsUdemyCourse/app/src/main/java/com/colinknecht/contactsudemycourse/EditContacts.java
@@ -1,7 +1,10 @@
 package com.colinknecht.contactsudemycourse;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +24,10 @@ public class EditContacts extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_contacts);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Contact Details");
+
+
         tvName = (TextView) findViewById(R.id.tv_name_edit_contact);
         tvPhone = (TextView) findViewById(R.id.tv_phone_edit_contact);
         btnEditContact = (Button) findViewById(R.id.btn_edit_contact);
@@ -39,8 +46,35 @@ public class EditContacts extends AppCompatActivity {
         ivCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+
             }
         });
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate the menu.. this adds items to the action bar if present
+        getMenuInflater().inflate(R.menu.menu_edit_contacts, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //handle action bar item clicks here..the action bar will automatically handle
+        // clicks on the home/up button, so long as you specify a parent activity in the AndroidManifest.xml
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_edit: {
+                return true;
+            }
+            case R.id.action_delete: {
+                return true;
+            }
+            default: {
+                return onOptionsItemSelected(item);
+            }
+        }//switch
+        
+    }
+}//end class
