9436e8a6b25794197f76cd7906e0b503b3a495a0
ColinKnecht <cknecht87@gmail.com>
2017-07-03 11:28:36 -0400

    Before Video 31

--- ContactsUdemyCourse/app/src/main/java/com/colinknecht/contactsudemycourse/EditContacts.java
+++ ContactsUdemyCourse/app/src/main/java/com/colinknecht/contactsudemycourse/EditContacts.java
@@ -2,12 +2,45 @@ package com.colinknecht.contactsudemycourse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class EditContacts extends AppCompatActivity {
+    private static final String TAG = "EditContacts";
+
+    TextView tvName, tvPhone;
+    Button btnEditContact;
+    EditText etFirstName, etLastName, etPhoneNumber;
+    ImageView ivCall;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_contacts);
+
+        tvName = (TextView) findViewById(R.id.tv_name_edit_contact);
+        tvPhone = (TextView) findViewById(R.id.tv_phone_edit_contact);
+        btnEditContact = (Button) findViewById(R.id.btn_edit_contact);
+        etFirstName = (EditText) findViewById(R.id.et_firstname_edit_contact);
+        etLastName = (EditText) findViewById(R.id.et_lastname_edit_contact);
+        etPhoneNumber = (EditText) findViewById(R.id.et_phone_edit_contact);
+        ivCall = (ImageView) findViewById(R.id.iv_call);
+
+        btnEditContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        ivCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
