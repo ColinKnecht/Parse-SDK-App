9d098d85e3aeba7be500a966cb615e85ade205ea
ColinKnecht <cknecht87@gmail.com>
2017-07-04 10:46:26 -0400

    Before Video 38

--- ContactsUdemyCourse/app/src/main/java/com/colinknecht/contactsudemycourse/EditContacts.java
+++ ContactsUdemyCourse/app/src/main/java/com/colinknecht/contactsudemycourse/EditContacts.java
@@ -1,12 +1,15 @@
 package com.colinknecht.contactsudemycourse;
 
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,6 +19,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.DeleteCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -151,6 +155,68 @@ public class EditContacts extends AppCompatActivity {
                 return true;
             }
             case R.id.action_delete: {
+                LayoutInflater inflater = getLayoutInflater();
+                final View layout = inflater.inflate(R.layout.are_you_sure_popup,null);//inflates the xml layout
+
+                final TextView tvDeleteSure = (TextView) layout.findViewById(R.id.tv_delete_sure);
+                tvDeleteSure.setText("Do You Want to Delete " + getIntent().getStringExtra("firstName") + " " + getIntent().getStringExtra("lastName")+ "?");
+
+                final AlertDialog.Builder dlgDelete = new AlertDialog.Builder(EditContacts.this);
+                dlgDelete.setView(layout);
+                dlgDelete.setTitle("Are You Sure?");
+
+                dlgDelete.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        final ProgressDialog progressDialog = new ProgressDialog(EditContacts.this);
+                        progressDialog.setMessage("Busy Deleting Contact....Please Wait");
+                        progressDialog.show();
+
+                        ParseQuery<ParseObject> query = ParseQuery.getQuery("Contacts");
+                        query.getInBackground(getIntent().getStringExtra("id"), new GetCallback<ParseObject>() {
+                            @Override
+                            public void done(ParseObject object, ParseException e) {
+                                if (e == null) {
+                                    object.deleteInBackground(new DeleteCallback() {
+                                        @Override
+                                        public void done(ParseException e) {
+
+                                            if (e == null) {
+                                                Toast.makeText(EditContacts.this, "Delete Successful", Toast.LENGTH_SHORT).show();
+                                                progressDialog.dismiss();
+                                                EditContacts.this.finish();
+                                                Intent intent = new Intent(EditContacts.this, ContactList.class);
+                                                startActivity(intent);
+                                            }
+                                            else {
+                                                Toast.makeText(EditContacts.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                                Log.e(TAG, "done: " + e.getMessage() );
+                                                progressDialog.dismiss();
+                                                EditContacts.this.finish();
+                                                Intent intent = new Intent(EditContacts.this, ContactList.class);
+                                                startActivity(intent);
+                                            }
+                                        }
+                                    });
+                                }
+                                else {
+                                    Toast.makeText(EditContacts.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    progressDialog.dismiss();
+                                }
+                            }
+                        });
+                    }
+                });
+
+                dlgDelete.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        
+                    }
+                });
+
+                dlgDelete.show();
+
                 return true;
             }
             default: {
